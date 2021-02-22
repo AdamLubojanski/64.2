772e834ed4bf4e47b6d1eba7709948570275501f
AdamLubojanski <62133278+AdamLubojanski@users.noreply.github.com>
2021-02-22 10:07:24 +0100

    Create 642.cpp

---
+++ 642.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+ifstream in("dane_obrazki.txt");//otwarcie pliku 
+bool isRecurent() {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      if (tab[i][j] != tab[i][j + 10] || tab[i][j] != tab[i + 10][j] || 
+          tab[i][j] != tab[i+10][j+10]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+void obrazek() {
+  for (int i = 0; i < 20; i++) {
+    for (int j = 0; j < 20; j++) {
+      cout << tab[i][j];
+    }
+    cout << endl;
+  }
+  cout << endl;
+}
+
+int main(int argc, char** argv) {
+int recurentCounter = 0;
+bool recurentFlag = true;
+obrazek();
+recurentFlag = false;
+cout << recurentCounter
+return 0;//koniec
+}
